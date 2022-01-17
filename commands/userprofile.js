5592d3ae30591dfa4bf642b39f2965e67cb3e235
Dylan Wintermute <dylan.wintermute.17@cnu.edu>
2022-01-17 09:33:50 -0500

    added admin userprofile

---
+++ commands/userprofile.js
@@ -0,0 +1,39 @@
+var maids = require("../units/maids.json");
+const playerModel = require("../models/playerSchema");
+const { userMention, memberNicknameMention, channelMention, roleMention } = require('@discordjs/builders');
+module.exports = {
+    name: 'userprofile',
+    aliases: [],
+    permissions: ["ADMINISTRATOR"],
+    description: "embeds",
+    async execute(client, message,cmd,args,Discord){
+        if (args.length === 0) return message.reply("Please enter a user ID");
+        let ID = args[0];
+        let playerData; 
+        playerData = await playerModel.findOne({ userID: ID});
+        if (!playerData) return message.channel.send("You don't exist. Please try again.");
+        if (playerData.starterSelected === false) return message.reply("You need to run g$register first before anything else");
+        let totalCP = playerData.totalCP;
+        let totalCoins = playerData.coins;
+        var user = await client.users.fetch(ID);
+       
+        const newEmbed = new Discord.MessageEmbed()
+        .setColor('#E76AA3')
+        .setAuthor(`${user.username}'s Condensed Profile`)
+        .setDescription(`**Total CP:** ${new Intl.NumberFormat().format(totalCP)} 
+        **Total<:bootaomonez:909294739197681754>:** ${new Intl.NumberFormat().format(totalCoins)} 
+        **Raid Tickets:** ${playerData.raidTickets} 
+        **Raids Won:** ${playerData.raidsWon} 
+        **Weekly Raids Won:** ${playerData.weeklyRaidsWon}
+        **Damage Done to Mega Boss:** ${new Intl.NumberFormat().format(playerData.megaRaidDamageDone)}`)
+
+        
+        
+        
+        message.channel.send({ embeds: [newEmbed] });
+        
+        
+        
+    }
+    
+}
